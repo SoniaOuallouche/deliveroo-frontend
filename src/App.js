c9abc0d75ca7b1c64aeda8ad6f13b62917162025
Sonia Ouallouche <sonia@air-de-sonia.lan>
2022-02-08 16:11:15 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import Hero from "./components/Hero";
+import Category from "./components/Category";
+
+function App() {
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://deliveroo-backendd.herokuapp.com/"
+        );
+        console.log(response.data);
+        setData(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error.response);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const handleClick = (index, action) => {};
+
+  return isLoading ? (
+    <div>En cours de chargement...</div>
+  ) : (
+    <div>
+      <Hero restaurant={data.restaurant} />
+      {/* Liste de catégories */}
+      {data.categories.map((category, index) => {
+        return (
+          category.meals.length > 0 && (
+            <Category key={index} category={category} />
+          )
+        );
+      })}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Category.js
@@ -0,0 +1,28 @@
+const Category = ({ category }) => {
+  return (
+    <div>
+      <h2>{category.name}</h2>
+      {/* List de produits */}
+      {category.meals.map((meal, index) => {
+        return (
+          <div
+            onClick={() => {
+              console.log("J'ai cliqué sur le produit ====> ", meal);
+            }}
+            className="card"
+          >
+            <h3>{meal.title}</h3>
+            <p>{meal.description.slice(0, 50)}...</p>
+            {meal.picture && (
+              <img style={{ height: 100 }} src={meal.picture} alt="" />
+            )}
+            <span>{meal.price} €</span>
+            {meal.popular && <span>★ Populaire</span>}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Category;
